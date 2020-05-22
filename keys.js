2cc2d1ae9f440818c53e875b8ee45dbacf90ca9c
MarioFrausto <61289063+MarioFrausto@users.noreply.github.com>
2020-05-22 15:46:16 -0500

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'sql10.freemysqlhosting.net',
+        user: 'sql10342005',
+        password: '7gp7PIA5U9',
+        database: 'sql10342005'
+    }
+
+};
